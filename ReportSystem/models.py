de8db00cc2955181052827bb936a7c2e3dc11a43
Toluwalemi <toluwalemisrael@gmail.com>
2020-01-24 00:39:43 +0100

    add approve manager model

--- ReportSystem/models.py
+++ ReportSystem/models.py
@@ -1,11 +1,12 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
 
 class Reporter(models.Model):
-    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reporters')
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='reporters')
     address = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=14)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,7 +17,7 @@ class Reporter(models.Model):
 
 
 class Crime(models.Model):
-    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reporters')
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, related_name='reporters')
     crime_type = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     location = models.CharField(max_length=50)
@@ -28,14 +29,14 @@ class Crime(models.Model):
 
 
 class Station(models.Model):
-    name = models.CharField(max_length=50, primary_key=True, unique=True)
+    name = models.CharField(max_length=50, primary_key=True)
     location = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         indexes = [models.Index(fields=['name'])]
-        ordering = ['-name']
+        ordering = ['-name']  # descending order
 
     def __str__(self):
         return self.name
@@ -58,14 +59,33 @@ class Security(models.Model):
         return self.officer.get_full_name()
 
 
+class ApprovedManager(models.Manager):
+    """function to return all approved reports"""
+
+    def get_queryset(self):
+        return super(ApprovedManager, self).get_queryset().filter(status='STATUS_APPROVE')
+
+
 class Report(models.Model):
+    STATUS_PROCESS = 'P'
+    STATUS_FAIL = 'F'
+    STATUS_APPROVE = 'A'
+    STATUS_CHOICES = [
+        (STATUS_PROCESS, 'Processing'),
+        (STATUS_FAIL, 'Failed'),
+        (STATUS_APPROVE, 'Approved'),
+    ]
     subject = models.CharField(max_length=30)
     details = models.TextField()
     crime = models.ForeignKey(Crime, on_delete=models.CASCADE, related_name='crimes')
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, related_name='reporters')
     security = models.ForeignKey(Security, on_delete=models.CASCADE, related_name='securities')
+    approve = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=STATUS_PROCESS)
+    objects = models.Manager()  # The default manager
+    approved = ApprovedManager()  # My custom manager
 
     def __str__(self):
         return self.subject
