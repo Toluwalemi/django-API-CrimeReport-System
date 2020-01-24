8a0b56c965c3180f1d3780adf8e3597daf3ad4d3
Toluwalemi <toluwalemisrael@gmail.com>
2020-01-24 01:44:00 +0100

    fix($compile): include unique=True for phone_number
    Phone numbers of Reporters and Securities should be unique in the
    database

--- ReportSystem/models.py
+++ ReportSystem/models.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 class Reporter(models.Model):
     reporter = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='reporters')
     address = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=14)
+    phone_number = models.CharField(max_length=14, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,7 +17,7 @@ class Reporter(models.Model):
 
 
 class Crime(models.Model):
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, related_name='reporters')
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
     crime_type = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     location = models.CharField(max_length=50)
@@ -46,7 +46,7 @@ class Security(models.Model):
     crime = models.ForeignKey(Crime, on_delete=models.CASCADE, related_name='crimes')
     officer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='officers')
     address = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=14)
+    phone_number = models.CharField(max_length=14, unique=True)
     station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='stations')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -77,9 +77,9 @@ class Report(models.Model):
     ]
     subject = models.CharField(max_length=30)
     details = models.TextField()
-    crime = models.ForeignKey(Crime, on_delete=models.CASCADE, related_name='crimes')
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, related_name='reporters')
-    security = models.ForeignKey(Security, on_delete=models.CASCADE, related_name='securities')
+    crime = models.ForeignKey(Crime, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    security = models.ForeignKey(Security, on_delete=models.CASCADE)
     approve = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
