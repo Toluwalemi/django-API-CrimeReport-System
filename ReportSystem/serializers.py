46af51f40bd68b40ac1c95158a03e0b99097122d
Toluwalemi <toluwalemisrael@gmail.com>
2020-01-28 15:20:41 +0100

    feat: Added serializers.py
    Added serializers.py and included rest_framework settings in settings.py

---
+++ ReportSystem/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from .models import Reporter, Crime, Station, Security, Report
+
+
+class ReporterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reporter
+        fields = ('id', 'reporter', 'address', 'phone_number', 'created_at')
+
+
+class CrimeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crime
+        fields = ('id', 'reporter', 'crime_type', 'description', 'location', 'created_at')
+
+
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ('id', 'name', 'location', 'created_at')
+
+
+class SecuritySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Security
+        fields = ('id', 'crime', 'officer', 'address', 'phone_number', 'station', 'created_at')
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = ('id', 'subject', 'details', 'crime', 'reporter', 'security', 'approve', 'status', 'created_at')
--- project/settings.py
+++ project/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ReportSystem.apps.ReportsystemConfig',
+
+    # Framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -81,7 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -101,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -115,8 +113,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST FRAMEWORK
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
