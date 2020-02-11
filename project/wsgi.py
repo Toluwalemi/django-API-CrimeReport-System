7c297d7535b3b6f9f0fc386c8453bbba03e203e3
Toluwalemi <toluwalemisrael@gmail.com>
2020-02-11 16:08:54 +0100

    style: Remove unnecessary lines fom wsgi.py file

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-# from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
